55259d3bfb30f4af557e510de8ba14c252519e54
Shishir Kharel <shishir167@gmail.com>
2015-07-22 11:35:35 -0400

    Added support for ios

--- www/js/index.js
+++ www/js/index.js
@@ -34,7 +34,15 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
-        app.initPushwoosh();
+        if (device.platform == "Android") {
+            app.initPushwooshAndroid();
+        }
+
+        if (device.platform == "iPhone" || device.platform == "iOS") {
+            app.initPushwooshIOS();
+        }
+
+
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -48,7 +56,45 @@ var app = {
         console.log('Received Event: ' + id);
     },
 
-    initPushwoosh: function() {
+    initPushwooshIOS: function() {
+        alert("ios detected");
+        var pushNotification = cordova.require("com.pushwoosh.plugins.pushwoosh.PushNotification");
+
+        //set push notification callback before we initialize the plugin
+        document.addEventListener('push-notification', function(event) {
+            //get the notification payload
+            var notification = event.notification;
+
+            //display alert to the user for example
+            alert(notification.aps.alert);
+
+            //clear the app badge
+            pushNotification.setApplicationIconBadgeNumber(0);
+        });
+
+        //initialize the plugin
+        pushNotification.onDeviceReady({
+            pw_appid: "18196-066AC"
+        });
+
+        //register for pushes
+        pushNotification.registerDevice(
+            function(status) {
+                var deviceToken = status['deviceToken'];
+                console.warn('registerDevice: ' + deviceToken);
+            },
+            function(status) {
+                console.warn('failed to register : ' + JSON.stringify(status));
+                alert(JSON.stringify(['failed to register ', status]));
+            }
+        );
+
+        //reset badges on app start
+        pushNotification.setApplicationIconBadgeNumber(0);
+    },
+
+    initPushwooshAndroid: function() {
+        alert("android detected");
         var pushNotification = cordova.require("com.pushwoosh.plugins.pushwoosh.PushNotification");
 
         //set push notifications handler
@@ -76,7 +122,7 @@ var app = {
         pushNotification.registerDevice(
             function(status) {
                 var pushToken = status;
-                //alert('push token: ' + pushToken);
+                alert('push token: ' + pushToken);
             },
             function(status) {
                 alert(JSON.stringify(['failed to register ', status]));
